94a3acf8869622e54369d8edec8077f97e42c30c
Lucas Taliberti de Sousa <lucastaliberti@gmail.com>
2020-04-05 19:49:14 -0700

    app organization

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
+import * as path from "path";
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { NearbyPlacesModule } from './nearby-places/nearby-places.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: path.join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+    NearbyPlacesModule,
+  ],
 })
 export class AppModule {}
