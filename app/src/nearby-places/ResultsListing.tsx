68d1b493a328c0e76ea9a6ed55d129464b2ce0c3
Lucas Taliberti de Sousa <lucastaliberti@gmail.com>
2020-04-07 22:04:09 -0700

    Results table layout

--- app/src/nearby-places/ResultsListing.tsx
+++ app/src/nearby-places/ResultsListing.tsx
@@ -7,7 +7,13 @@ interface ResultsListingProps {
   location?: string;
   loading: boolean;
   error?: { message: string };
-  data?: object;
+  data?: {
+    getPlaces: Array<{
+      id: string;
+      name: string;
+      formattedAddress: string[];
+    }>;
+  };
 }
 
 function ResultsListing({
@@ -31,8 +37,24 @@ function ResultsListing({
           <ListName>Nearby places to</ListName>
           <LocationName>{location}</LocationName>
         </CityLabelHeader>
-
-        {JSON.stringify(data, null, 2)}
+        <Table>
+          <thead>
+            <tr>
+              <TableHeadCell>Name</TableHeadCell>
+              <TableHeadCell>Address</TableHeadCell>
+            </tr>
+          </thead>
+          <tbody>
+            {data.getPlaces.map(venue => (
+              <TableDataRow key={venue.id}>
+                <TableDataCell>{venue.name}</TableDataCell>
+                <TableDataCell>
+                  {venue.formattedAddress && venue.formattedAddress.join(", ")}
+                </TableDataCell>
+              </TableDataRow>
+            ))}
+          </tbody>
+        </Table>
       </>
     );
   }
@@ -59,5 +81,24 @@ const LocationName = styled.span`
   padding: 0 10px;
   position: relative;
 `;
+
+const Table = styled.table`
+  margin: 30px auto 0;
+  color: #4e595d;
+`;
+
+const TableDataRow = styled.tr`
+  border-top: 1px solid #efeff4;
+`;
+
+const TableDataCell = styled.td`
+  height: 40px;
+  padding: 10px 20px;
+  vertical-align: middle;
+`;
+
+const TableHeadCell = styled(TableDataCell)`
+  font-weight: bold;
+`
 
 export default ResultsListing;
