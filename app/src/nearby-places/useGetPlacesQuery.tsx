5e8a7506196fbf1751147012e1060eea8f8afeaa
Lucas Taliberti de Sousa <lucastaliberti@gmail.com>
2020-04-07 21:12:38 -0700

    Basic graphql client and hook

---
+++ app/src/nearby-places/useGetPlacesQuery.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+export function useGetPlacesQuery(location: string) {
+  const GET_PLACES_QUERY = gql`
+    query GetPlaces($location: String!) {
+      getPlaces(geocodableLocation: $location) {
+        id
+        name
+        formattedAddress
+      }
+    }
+  `;
+
+  return useQuery(GET_PLACES_QUERY, {
+    variables: { location }
+  });
+}
